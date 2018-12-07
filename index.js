865a6cc345a15c0c3e334b25c65634156ff3ac47
Andrew Nguyen <ashewx@gmail.com>
2018-12-07 15:34:12 -0700

    Fix

--- index.js
+++ index.js
@@ -44,26 +44,29 @@ function WebhookProcessing(req, res) {
 			console.log(agent.session);
 			break;
 
-		case "WebPT Conversation":
-
-			break;
-
 		case "print-form":
 
 			break;
 
 		case "get-pt":
 			// SQL select doctor first name, last name, id for the patient's physical therapist
+			function query() {
+
+			}
 			text = 'SELECT d.fname, d.lname, d.doctorid FROM doctors AS d, patients AS p, goesto AS g WHERE' + patient_id + ' = g.patientid AND d.doctorid = g.doctorid';
 			let pt_info = null;
 			client.query(text).then(response => {
 				console.log(response.rows[0]);
 				pt_info = response.rows[0];
         //see log for output
-      }).catch(e => {console.error(e.stack); ssml = '<speak>Unable to find Physical Therapist info for patient ' + patient_id + '<speak>';});
-			let pt_name = pt_info.fname + ' ' + pt_info.lname;  // first name + ' ' + last name
-			pt_id = pt_info.doctorid;
-			ssml = '<speak>Your Physical Therapist is ' + pt_name + '<speak>';
+      }).then(function() {
+				if (pt_info !== null) {
+					let pt_name = pt_info.fname + ' ' + pt_info.lname;  // first name + ' ' + last name
+					pt_id = pt_info.doctorid;
+					ssml = '<speak>Your Physical Therapist is ' + pt_name + '<speak>';
+				}
+			}).catch(e => {console.error(e.stack); ssml = '<speak>Unable to find Physical Therapist info for patient ' + patient_id + '<speak>';});
+
 			break;
 
 		case "set-pt":
@@ -71,18 +74,18 @@ function WebhookProcessing(req, res) {
 			// get physical therapist for current user
 			// pt_id = "SELECT d.id FROM doctors AS d WHERE d.name = " + agent.parameters
 			// INSERT INTO goesto VALUES(patient_id, pt_id);
-			text = 'SELECT d.doctorid FROM doctors AS d WHERE d.fname = ' + agent.parameters['first-name'] + ' AND d.lname = ' + agent.parameters['last-name'];
 			let pt_id = null;
-			client.query(text).then(response => {
+			client.query('SELECT d.doctorid FROM doctors AS d WHERE d.fname = ' + agent.parameters['first-name'] + ' AND d.lname = ' + agent.parameters['last-name']).then(response => {
 				console.log(response.rows[0]);
 				pt_id = response.rows[0]['doctorid'];
-      }).catch(e => {console.error(e.stack); ssml = '<speak>Unable to get Physical Therapist ' + agent.parameters['first-name'] + ' ' + agent.parameters['last-name'] + '<speak>';});
-			text = 'INSERT INTO goesto VALUES(' + patient_id + ', ' + pt_id + ')';
-			client.query(text).then(response => {
-				console.log(response.rows[0]);
-				pt_id = response.rows[0];
-      }).catch(e => {console.error(e.stack); ssml = '<speak>Unable to set Physical Therapist for patient ' + patient_id + '<speak>';});
-			ssml = '<speak>Your Physical Therapist was set to ' + agent.parameters['first-name'] + ' ' + agent.parameters['last-name'] + '<speak>';
+      }).then(function () {
+				client.query('INSERT INTO goesto VALUES(' + patient_id + ', ' + pt_id + ')').then(response => {
+					console.log(response.rows[0]);
+					pt_id = response.rows[0];
+	      }).catch(e => {console.error(e.stack); ssml = '<speak>Unable to set Physical Therapist for patient ' + patient_id + '<speak>';});
+			}).then(function() {
+				ssml = '<speak>Your Physical Therapist was set to ' + agent.parameters['first-name'] + ' ' + agent.parameters['last-name'] + '<speak>';
+			}).catch(e => {console.error(e.stack); ssml = '<speak>Unable to get Physical Therapist ' + agent.parameters['first-name'] + ' ' + agent.parameters['last-name'] + '<speak>';});
 			break;
 
 		case "check-missing":
