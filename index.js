688ee732eca8276aa904f4134a243b301d1e83b8
Andrew Nguyen <ashewx@gmail.com>
2018-12-07 15:15:22 -0700

    Fix

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function WebhookProcessing(req, res) {
 	let origMess = agent.consoleMessages[agent.consoleMessages.length-1].text;
 	console.log(origMess);
 	var ssml = `<speak>` + errorSound + `</audio>` + `</speak>`;
+	let text = null;
 
 	switch(intent){
 		case "Welcome Intent":
@@ -53,7 +54,7 @@ function WebhookProcessing(req, res) {
 
 		case "get-pt":
 			// SQL select doctor first name, last name, id for the patient's physical therapist
-			let text = 'SELECT d.fname, d.lname, d.doctorid FROM doctors AS d, patients AS p, goesto AS g WHERE' + patient_id + ' = g.patientid AND d.doctorid = g.doctorid';
+			text = 'SELECT d.fname, d.lname, d.doctorid FROM doctors AS d, patients AS p, goesto AS g WHERE' + patient_id + ' = g.patientid AND d.doctorid = g.doctorid';
 			let pt_info = null;
 			client.query(text).then(response => {
 				console.log(response.rows[0]);
@@ -70,14 +71,14 @@ function WebhookProcessing(req, res) {
 			// get physical therapist for current user
 			// pt_id = "SELECT d.id FROM doctors AS d WHERE d.name = " + agent.parameters
 			// INSERT INTO goesto VALUES(patient_id, pt_id);
-			let text = 'SELECT d.doctorid FROM doctors AS d WHERE d.fname = ' + agent.parameters['first-name'] + ' AND d.lname = ' + agent.parameters['last-name'];
+			text = 'SELECT d.doctorid FROM doctors AS d WHERE d.fname = ' + agent.parameters['first-name'] + ' AND d.lname = ' + agent.parameters['last-name'];
 			let pt_id = null;
 			client.query(text).then(response => {
 				console.log(response.rows[0]);
 				pt_id = response.rows[0]['doctorid'];
       }).catch(e => {console.error(e.stack); ssml = '<speak>Unable to get Physical Therapist ' + agent.parameters['first-name'] + ' ' + agent.parameters['last-name'] + '<speak>';});
-			let text2 = 'INSERT INTO goesto VALUES(' + patient_id + ', ' + pt_id + ')';
-			client.query(text2).then(response => {
+			text = 'INSERT INTO goesto VALUES(' + patient_id + ', ' + pt_id + ')';
+			client.query(text).then(response => {
 				console.log(response.rows[0]);
 				pt_id = response.rows[0];
       }).catch(e => {console.error(e.stack); ssml = '<speak>Unable to set Physical Therapist for patient ' + patient_id + '<speak>';});
