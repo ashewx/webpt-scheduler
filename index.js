26f03273bec59825519fdb3f1057ae4c28ac248b
Andrew Nguyen <ashewx@gmail.com>
2018-12-07 17:17:25 -0700

    Fix

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const successSound = `<audio src="https://notificationsounds.com/message-tones/f
 // Test Variables
 const patient_id = 1;
 var pt_id = 0;
+var pt_name = null;
 
 function WebhookProcessing(req, res) {
 	 //Create an instance
@@ -54,7 +55,7 @@ function WebhookProcessing(req, res) {
 					console.log(response.rows[0]);
 					pt_info = response.rows[0];
 					if (pt_info !== null) {
-						let pt_name = pt_info.fname + ' ' + pt_info.lname;  // first name + ' ' + last name
+						pt_name = pt_info.fname + ' ' + pt_info.lname;  // first name + ' ' + last name
 						pt_id = pt_info.doctorid;
 						// console.log(pt_id);
 						agent.add(`<speak>Your Physical Therapist is ` + pt_name + `</speak>`);
@@ -66,10 +67,52 @@ function WebhookProcessing(req, res) {
 			break;
 
 		case "set-pt":
+
+
+			break;
+		case "schedule-appointment":
+			respond = function(agent) {
+				text = 'SELECT d.fname, d.lname, d.doctorid FROM doctors AS d, patients AS p, goesto AS g WHERE ' + patient_id + ' = g.patientid AND d.doctorid = g.doctorid';
+				let pt_info = null;
+				return client.query(text).then(response => {
+					console.log(response.rows[0]);
+					pt_info = response.rows[0];
+					if (pt_info !== null) {
+						pt_name = pt_info.fname + ' ' + pt_info.lname;  // first name + ' ' + last name
+						pt_id = pt_info.doctorid;
+						// console.log(pt_id);
+						agent.add(`<speak>Your current physical therapist is ` + pt_name + `. Is this correct?</speak>`);
+					}
+				}).catch(e => {
+					console.log(e.stack); ssml = `<speak>Unable to find Physical Therapist info for patient ` + patient_id + `</speak>`;
+				});
+			}
 		  break;
 
-		case "schedule-appointment":
-		  break;
+		case "appointment-times":
+			respond = function(agent) {
+				text = 'SELECT a.appid FROM doctors AS d, appointments AS a, schedule AS s WHERE ' + pt_id + ' = s.doctorid AND ' + agent.parameters['date'] + ' = a.appdate AND ' + agent.parameters['time'] + ' = a.apptime';
+				let app_info = null;
+				return client.query(text).then(response => {
+					console.log(response.rows[0]);
+					app_info = response.rows[0];
+					if (app_info !== null) {
+						agent.add(`<speak>Unfortunately ` + pt_name + ` is busy during that time. Is there another time that works for you?</speak>`);
+					}
+					else {
+						// GET LAST app key
+						client.query('SELECT appid FROM appointments ORDER BY DESC appid LIMIT 1').then(response1 => {
+							let last_app_key = response1.rows[0].appid;
+							let text2 = 'INSERT INTO apptime VALUES(' + last_app_key + ', ' + agent.parameters['date'] + ', ' + agent.parameters['time'] + '); INSERT INTO schedule VALUES(' + pt_id + ', ' + last_app_key + ');';
+							client.query(text2);
+						});
+						agent.add(`<speak>Great! I will add you to ` + pt_name + `'s schedule for ` + agent.parameters['date'] + ` at ` + agent.parameters['time'] + `.</speak>`);
+					}
+				}).catch(e => {
+					console.log(e.stack);
+				});
+			}
+			break;
 
     case "play-sound":
 		  ssml = `<speak> Error Sound` + errorSound  + ` ...</audio>` +
